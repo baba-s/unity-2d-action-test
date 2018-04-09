660143e914bbe15d8da473290a50f325571740af
baba_s <shotababa1206@gmail.com>
2018-04-09 17:57:21 +0900

    初コミット

---
+++ Assets/_Project/Scripts/FragileBlock.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class FragileBlock : MonoBehaviour
+{
+	public float m_timeDisableCollider;
+	public float m_timeDestroy;
+
+	private Animator m_animator;
+	private bool m_isDestroyed;
+	
+	private void Awake()
+	{
+		var motor = GetComponent<MovingPlatformMotor2D>();
+		motor.onPlatformerMotorContact += OnPlatformerMotorContact;
+
+		m_animator = GetComponent<Animator>();
+	}
+
+	private void OnPlatformerMotorContact( PlatformerMotor2D player )
+	{
+		if ( m_isDestroyed ) return;
+		m_isDestroyed = true;
+
+		Invoke( "DisableCollider", m_timeDisableCollider );
+
+		Destroy( gameObject, m_timeDestroy );
+		m_animator.SetBool( "IsHit", true );
+	}
+
+	private void DisableCollider()
+	{
+		GetComponent<Collider2D>().enabled = false;
+	}
+}
